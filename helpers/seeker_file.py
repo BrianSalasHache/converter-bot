b1c3253a79276cdf4e1c36e0e2b6b772142c69f7
BrianSalasHache <brian_mdr_97@hotmail.com>
2022-03-18 16:36:48 -0300

    added audio_converter in audios.py file

--- helpers/seeker_file.py
+++ helpers/seeker_file.py
@@ -5,9 +5,7 @@ def seeker(ext1, *args):
     list = []
     for i in enumerate(path):
         r = i[1].rsplit('.', 1)
-        name = r[0]
-        ext = r[-1]
-        print(name, ext)
+        name, ext = r
         if ext == ext1 and not args:
             list.append(i[1])
         if args and ext == ext1:
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from os import system
 
+from converters.audios import audios_options
 from converters.documents import documents_options
 from converters.images import images_options
 
 option = ''
 while option != '0':
     system('cls')
-    option = input('QUE QUERES CONVERTIR?\n\n1. Documento\n2. Imagen\n\n0. Salir del programa\n\n')
+    option = input('QUE QUERES CONVERTIR?\n\n1. Audio\n2. Documento\n3. Imagen\n\n0. Salir del programa\n\n')
     system('cls')
     if option == '1':
-        documents_options()
+        audios_options()
     elif option == '2':
+        documents_options()
+    elif option == '3':
         images_options()
